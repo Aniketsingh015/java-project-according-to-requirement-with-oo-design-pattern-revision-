db94f56e303f9e1e20eb197c821504e12827c6be
Aniket Singh <asaniket.singh422@gmail.com>
2026-09-06 16:59:43 +0530

    order start code

---
+++ Order.java
@@ -0,0 +1,10 @@
+public class Order {
+    private int orderId;
+
+    public void setOrderId(int orderId){
+        this.orderId=orderId;
+    }
+    public int getOrderId(){
+        return this.orderId;
+    }
+}
