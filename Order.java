f288440441b2577049b84cde0d4eb9d8cbcaa3f9
Aniket Singh <asaniket.singh422@gmail.com>
2026-09-06 20:17:54 +0530

    update

--- Order.java
+++ Order.java
@@ -1,6 +1,8 @@
+import java.util.*;
 public class Order {
     private int orderId;
     private Customer customer;//has -a relationship with backward linking of customer as 1 to many with unidirectional
+    private List<OrderItem>orderItem;
 
     public void setOrderId(int orderId){
         this.orderId=orderId;
@@ -14,5 +16,16 @@ public class Order {
     }
     public void setCustomer(Customer customer){
         this.customer=customer;
+    }
+
+    public List<OrderItem> getOrderItems(){
+        return this.orderItem;
+    }
+    public void addOrderItem(OrderItem orderItem){
+        this.orderItem.add(orderItem);
+    }
+
+    public Order(){
+        this.orderItem=new ArrayList<OrderItem>();
     }
 }
