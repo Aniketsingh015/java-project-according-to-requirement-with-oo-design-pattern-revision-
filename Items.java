00dc37d4be145e8da5992e1c56392093ce2f9a83
Aniket Singh <asaniket.singh422@gmail.com>
2026-09-06 16:59:23 +0530

    items start code

---
+++ Items.java
@@ -0,0 +1,38 @@
+public class Items {
+    private int itemNo ;
+    private String itemName;
+    private int itemRate;
+
+    // getter and setter for itemNo
+
+    public void setItemNo(int itemNo){
+        this.itemNo=itemNo;
+    }
+    public int getItemNo(){
+        return this.itemNo;
+    }
+
+    // getter and setter for itemName
+
+    public void setItemName(String itemName){
+        this.itemName=itemName;
+    }
+    public String getItemName(){
+        return this.itemName;
+    }
+
+    // getter and setter for rate
+
+    public void setItemRate(int itemRate){
+        this.itemRate=itemRate;
+    }
+    public int getItemRate(){
+        return this.itemRate;
+    }
+
+
+    public Items(){
+        
+    }
+
+}
