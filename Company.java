0c6dc4664a0d026fc6f382113f7e8a5a0dba4460
Aniket Singh <asaniket.singh422@gmail.com>
2026-09-06 16:58:37 +0530

    company start code

---
+++ Company.java
@@ -0,0 +1,17 @@
+class Company{
+    private String name;
+
+
+// getter and setters for the company 
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+
+// constructor for company 
+    public Company(){
+
+    }
+}
