99136c050c2aff54e212f97bc5afb715bcbc352b
Aniket Singh <asaniket.singh422@gmail.com>
2026-09-06 17:37:37 +0530

    new class

--- Order.java
+++ Order.java
@@ -1,6 +1,6 @@
 public class Order {
     private int orderId;
-    private Customer customer;
+    private Customer customer;//has -a relationship with backward linking of customer as 1 to many with unidirectional
 
     public void setOrderId(int orderId){
         this.orderId=orderId;
---
+++ RegisteredCustomer.java
@@ -0,0 +1,10 @@
+public class RegisteredCustomer extends Customer {
+    private int discount;
+
+    public int getDiscount(){
+        return this.discount;
+    }
+    public void setDiscount(int discount){
+        this.discount =discount;
+    }
+}
