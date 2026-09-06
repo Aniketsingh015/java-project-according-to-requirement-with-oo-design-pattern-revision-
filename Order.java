ff949f9f360f2963e697deb7e3f0c411e67cd957
Aniket Singh <asaniket.singh422@gmail.com>
2026-09-06 17:26:40 +0530

    order addded with customer

--- Order.java
+++ Order.java
@@ -1,10 +1,18 @@
 public class Order {
     private int orderId;
+    private Customer customer;
 
     public void setOrderId(int orderId){
         this.orderId=orderId;
     }
     public int getOrderId(){
         return this.orderId;
+    }
+
+    public Customer getCustomer(){
+        return this.customer;
+    }
+    public void setCustomer(Customer customer){
+        this.customer=customer;
     }
 }
