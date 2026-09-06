32088c58b3ad112da36ac82e30397b4bd49b1a2a
Aniket Singh <asaniket.singh422@gmail.com>
2026-09-06 16:59:10 +0530

    customer start code

---
+++ Customer.java
@@ -0,0 +1,26 @@
+public class Customer {
+    private int custName;
+    private int custId;
+
+
+    // gtter and setters for custId
+    public int getCustId(){
+        return this.custId;
+    }
+    public void setCustId(int custId){
+        this.custId=custId;
+    }
+
+    // getter and setter for name
+    public int getCustName(){
+        return this.custName;
+    }
+    public void setCustName(int custName){
+        this.custName=custName;
+    }
+
+    // constructor for customer
+    public Customer(){
+
+    }
+}
